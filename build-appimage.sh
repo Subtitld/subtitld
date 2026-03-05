bc2a084ce01ad97b102003824b1d87f668aba66d
Jonatã Bolzan Loss <jonata@jonata.org>
2026-03-05 16:09:49 -0300

    Fix pipeline

--- build-appimage.sh
+++ build-appimage.sh
@@ -47,4 +47,4 @@ wget -q https://github.com/AppImage/AppImageKit/releases/download/continuous/app
 chmod +x appimagetool-x86_64.AppImage
 
 # Build AppImage
-./appimagetool-x86_64.AppImage --appimage-extract-and-run $APP_DIR Subtitld-$VERSION-x86_64.AppImage
+ARCH=x86_64 ./appimagetool-x86_64.AppImage --appimage-extract-and-run $APP_DIR Subtitld-$VERSION-x86_64.AppImage
